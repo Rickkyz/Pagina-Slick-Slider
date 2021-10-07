638259d24ebf657b6f3ad8d43f616315264333d3
Rickkyz <rickkys@yahoo.com>
2021-10-07 17:56:53 -0300

    Versão não finalizada de página com slider de imagens

---
+++ js/main.js
@@ -0,0 +1,15 @@
+$('.slider-principal').vegas({
+    delay: 7000,
+    timer: false,
+    shuffle: true,
+    firstTransition: 'fade',
+    firstTransitionDuration: 5000,
+    animation: 'kenburnsUp',
+    animationDuration: 2000,
+    slides: [
+        { src: "./img/space01.jpg" },
+        { src: "./img/space02.jpg" },
+        { src: "./img/space03.jpg" }
+              
+    ]
+});
